acf109ec46c364b397c29309273064051cf0bbcc
Brad Ben Joya <bradbenjoya6@gmail.com>
2026-03-25 00:31:39 +0200

    Added a way to add custom shaders to objects

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -109,4 +109,69 @@ void Rectangle::updateVertices() {
                   m_vertices.data());
   glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
+
+void Rectangle::calculateShaders()
+{
+
+  // Cleanup existing resources if any
+  if (m_shaderProgram) glDeleteProgram(m_shaderProgram);
+  if (m_VAO) glDeleteVertexArrays(1, &m_VAO);
+  if (m_VBO) glDeleteBuffers(1, &m_VBO);
+  if (m_EBO) glDeleteBuffers(1, &m_EBO);
+
+  // Shader setup
+  m_vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  const char *vs = m_vertexShaderSource.c_str();
+  glShaderSource(m_vertexShader, 1, &vs, nullptr);
+  glCompileShader(m_vertexShader);
+
+  m_fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  const char *fs = m_fragmentShaderSource.c_str();
+  glShaderSource(m_fragmentShader, 1, &fs, nullptr);
+  glCompileShader(m_fragmentShader);
+
+  m_shaderProgram = glCreateProgram();
+  glAttachShader(m_shaderProgram, m_vertexShader);
+  glAttachShader(m_shaderProgram, m_fragmentShader);
+  glLinkProgram(m_shaderProgram);
+
+  glDeleteShader(m_vertexShader);
+  glDeleteShader(m_fragmentShader);
+
+  // VBO/VAO/EBO setup
+  glGenVertexArrays(1, &m_VAO);
+  glGenBuffers(1, &m_VBO);
+  glGenBuffers(1, &m_EBO);
+
+  glBindVertexArray(m_VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+  glBufferData(GL_ARRAY_BUFFER, m_vertices.size() * sizeof(float),
+               m_vertices.data(), GL_DYNAMIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, m_indices.size() * sizeof(GLuint),
+               m_indices.data(), GL_DYNAMIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
+
+  // Initial calculation based on window dimensions
+  updateVertices();
+}
+
+void Rectangle::setFrag(const std::filesystem::path& path)
+{
+  m_fragmentShaderSource = Shader(path);
+  calculateShaders();
+}
+
+void Rectangle::setVertex(const std::filesystem::path& path)
+{
+  m_vertexShaderSource = Shader(path);
+  calculateShaders();
+}
 } // namespace gpgl
