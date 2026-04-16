2ec43bdbbfa33876336827ed2feea112e552c0ca
Brad Ben Joya <bradbenjoya6@gmail.com>
2026-04-16 22:22:21 +0300

    Added compatability with macos

--- src/window.cpp
+++ src/window.cpp
@@ -17,6 +17,9 @@ Window::Window(const uint& width, const uint& height, std::string_view title) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
     m_window =
         glfwCreateWindow(m_width, m_height, m_title.data(), nullptr, nullptr);
