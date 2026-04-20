2b5994e1cfcab57da4f5d033b9d6a5f0dd435635
Brad Ben Joya <bradbenjoya6@gmail.com>
2026-04-20 11:32:01 +0400

    Updated gpgl example

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,30 @@
-
-#include "texture.h"
-#include "triangle.h"
-#include "rectangle.h"
 #include "window.h"
-
+#include "rectangle.h"
+#include "input.h"
 
 int main() {
-    // Example script of GPGL
     gpgl::Window window(800, 600, "GPGL Example");
-    gpgl::Rectangle rectangle(200.0f, 200.0f, window);
-    rectangle.setPosition(400.0f, 300.0f);
-
-    gpgl::Texture tex("images/dog.jpg");
-    rectangle.setTexture(tex);
+    window.setClearColor(gpgl::Color(100,100,100,255));
+    gpgl::InputHandler input(window);
+    gpgl::Rectangle rect(100,100,window);
+    rect.setPosition(window.getWidth() / 2,window.getHeight() / 2);
 
     while (!window.shouldWindowClose()) {
         window.processEvents();
         window.clear();
-        
-        rectangle.draw();
-        
+        if (input.isKeyDown(gpgl::Keys::A)) {
+            rect.setPosition(rect.getPositionX() + 5 , rect.getPositionY());
+        }
+        if (input.isKeyDown(gpgl::Keys::D)) {
+            rect.setPosition(rect.getPositionX() - 5 , rect.getPositionY());
+        }
+        if (input.isKeyDown(gpgl::Keys::W)) {
+            rect.setPosition(rect.getPositionX() , rect.getPositionY() + 5);
+        }
+        if (input.isKeyDown(gpgl::Keys::S)) {
+            rect.setPosition(rect.getPositionX(), rect.getPositionY() - 5);
+        }
+        rect.draw();
         window.display();
     }
 }
